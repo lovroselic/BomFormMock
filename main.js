2d6a44e785c1f93b8fd401e08a747dfb99192bda
lovroselic <lovro.selic@gmail.com>
2021-08-20 15:12:50 +0200

    stable basic, ref by mol

--- main.js
+++ main.js
@@ -14,13 +14,19 @@ console.clear();
  */
 
 var descriptors = ['Component', 'Ref'];
-var inputs = ['mol', 'mass', 'volume'];
+var inputs = ['factor', 'mol', 'mass', 'volume'];
 var properties = ['MW', 'density', 'assay'];
 var headers = [...descriptors, ...inputs, ...properties];
 
 var CALC = {
-    ratio(arg){},
-    mass(arg) { 
+    factor(arg) {
+        console.log(".....calc factor", arg);
+        let value = arg.selectedFactor / arg.referenceFactor;
+        if (Number.isNaN(value)) return "";
+        return value;
+    },
+    mass(arg) {
+        console.log(".....calc mass", arg);
         let value = parseFloat(arg.mol * arg.MW);
         if (Number.isNaN(value)) {
             value = parseFloat(arg.volume * arg.density);
@@ -28,12 +34,14 @@ var CALC = {
         if (Number.isNaN(value)) return "";
         return value / arg.assay;
     },
-    volume(arg) { 
+    volume(arg) {
+        console.log(".....calc volume", arg);
         let value = parseFloat(arg.mass / arg.density);
         if (Number.isNaN(value)) return "";
         return value;
     },
-    mol(arg) { 
+    mol(arg) {
+        console.log(".....calc mol", arg);
         let value = parseFloat(arg.mass / arg.MW);
         if (Number.isNaN(value)) return "";
         return value * arg.assay;
@@ -59,10 +67,10 @@ var APP = {
                 // name
                 html += `<td>${COMPONENTS[comp].name}</td>`;
                 //ref
-                html += `<td><input type="radio" name="reference" id="RefComponent${comp}" value = "RefComponent${comp}"></td>`;
+                html += `<td><input type="radio" name="reference" id="Ref_Component${comp}" value = "Ref_Component${comp}"></td>`;
                 //inputs
-                for (let [index, inp] of inputs.entries()) {
-                    html += `<td><input class="response" type="text" id = "Component${comp}Input${index}"></td>`;
+                for (let inp of inputs) {
+                    html += `<td><input class="response" type="text" id = "Component${comp}_${inp}"></td>`;
                 }
                 //properties
                 for (let prop of properties) {
@@ -88,10 +96,10 @@ var APP = {
                 // name
                 html += `<td>${PRODUCTS[comp].name}</td>`;
                 //ref
-                html += `<td><input type="radio" name="reference" id="RefProduct${comp}" value = "RefProduct${comp}"></td>`;
+                html += `<td><input type="radio" name="reference" id="Ref_Product${comp}" value = "Ref_Product${comp}"></td>`;
                 //inputs
-                for (let [index, inp] of inputs.entries()) {
-                    html += `<td><input class="response" type="text" id = "Product${comp}Input${index}"></td>`;
+                for (let inp of inputs) {
+                    html += `<td><input class="response" type="text" id = "Product${comp}_${inp}"></td>`;
                 }
                 //properties
                 for (let prop of properties) {
@@ -104,70 +112,104 @@ var APP = {
                 $("#table > tbody").append(html);
             }
             // first checked by default
-            $("#RefComponent1").prop("checked", true);
+            $("#Ref_Component1").prop("checked", true);
+            // first factor 1 by default
+            $("#Component1_factor").val(1);
         }
     },
     handle() {
-        let element = this;
+        //let value = parseFloat(this.value);
+        let id = this.id;
+        let row = id.substring(0, id.indexOf("_"));
+        let column = id.substring(id.indexOf("_") + 1);
+        let referenceBy = $('input[name=whichref]:checked').val();
+        console.log("HANDLE", id, row, column, "referenceBy", referenceBy);
+        let R = $('input[name=reference]:checked').val();
+        R = R.substring(R.indexOf("_") + 1);
+        console.log(".R", R);
+        let allResponses = $(".response");
+
+        for (let response of allResponses) {
+            if (response.id === id) continue; //this was entered
+            if (!response.id.startsWith(`${row}_`)) continue;
+            console.log("..", response.id);
+            let responseColumn = response.id.substring(id.indexOf("_") + 1);
+            let ARG = APP.packArguments(row, R, referenceBy);
+            console.log("....Column: ", responseColumn, " #### ");
+            $(`#${response.id}`).val(CALC[responseColumn](ARG));
+        }
+    },
+    /*handle() {
+        //let element = this;
         let value = parseFloat(this.value);
         let id = this.id;
+        let row = id.substring(0, id.indexOf("Input"));
+        console.log("row", row);
         let R = $('input[name=reference]:checked').val();
         let referenceBy = $('input[name=whichref]:checked').val();
-        console.log("handler", id, value, "R", R, referenceBy);
-        // all responses need to be processed
+        //console.log("handler", id, value, "R", R, referenceBy);
+        // all responses in a row to be processed
         let responses = $(".response");
+        console.log("responses", responses);
         var refIndex = inputs.indexOf(referenceBy);
-        for (let response of responses) {
-            console.log("response.id",response.id);
+        for (let response of responses) { 
             if (response.id === id) continue; //this was entered
             let inputIdentifier = parseInt(response.id.substring(response.id.indexOf("Input") + "Input".length), 10);
-            //if (referenceBy === "mass" && inputIdentifier === inputs.indexOf("mol")) continue; //
-            let ref = `${R.substring(3)}Input${refIndex}`;
             let rowIdentifier = response.id.substring(0, response.id.indexOf("Input"));
+            if (rowIdentifier !== row) continue; //
+            let ref = `${R.substring(3)}Input${refIndex}`;
             let selected = `${rowIdentifier}Input${refIndex}`;
-            console.log("selected, ref", selected, ref);
             let ratio = APP.getRatio(selected, ref);
-            //if (Number.isNaN(ratio)) continue; //can't calculate
-            let ARG = APP.packArguments(rowIdentifier);
-            ARG.ratio = ratio;
-            console.log("ratio", ratio);
-            let func = CALC[inputs[inputIdentifier]];
-            //console.log(".func", func);
-            //calc value
-            $(`#${response.id}`).val(func.call(null, ARG));
-            //
-            //break;
+            console.log("response.id", response.id);
+            if (inputIdentifier === inputs.indexOf("ratio")) {
+                $(`#${response.id}`).val(ratio || "");
+
+            } else {
+                //if (referenceBy === "mass" && inputIdentifier === inputs.indexOf("mol")) continue; //
+                let ARG = APP.packArguments(rowIdentifier);
+                ARG.ratio = ratio;
+                let func = CALC[inputs[inputIdentifier]];
+                //console.log(".func", func);
+                //calc value
+                $(`#${response.id}`).val(func.call(null, ARG));
+                //
+                //break;
+            }
+
         }
         //mass sum
         let massSum = 0;
-        for (let component = 1; component <= Object.keys(COMPONENTS).length; component++){
+        for (let component = 1; component <= Object.keys(COMPONENTS).length; component++) {
             massSum += parseFloat($(`#Component${component}Input${inputs.indexOf("mass")}`).val()) || 0;
         }
         $(`#S${inputs.indexOf("mass")}`).html(massSum);
 
         //if reference by mass fill massSum as target value
-        if (referenceBy === 'mass'){
+        if (referenceBy === 'mass') {
             //$("#Product1Input1").val(massSum); //direct, does not scale, refactor!!
             $(`#Product1Input${inputs.indexOf("mass")}`).val(massSum);
         }
 
-    },
+    },*/
     getRatio(selected, ref) {
         return parseFloat($(`#${selected}`).val()) / parseFloat($(`#${ref}`).val());
     },
-    packArguments(row){
+    packArguments(row, R, referenceBy) {
         let arg = {};
         //inputs
-        for (let [index, inp] of inputs.entries()){
-            let id = `${row}Input${index}`;
+        for (let inp of inputs) {
+            let id = `${row}_${inp}`;
             arg[inp] = parseFloat($(`#${id}`).val());
         }
         //properties
-        for (let [index, prop] of properties.entries()){
+        for (let prop of properties) {
             let id = `${row}_${prop}`;
             arg[prop] = parseFloat($(`#${id}`).html());
         }
         arg.assay = arg.assay || 1;
+        console.log(">>>", `#${R}_${referenceBy}`, `#${row}_${referenceBy}`);
+        arg.referenceFactor = parseFloat($(`#${R}_${referenceBy}`).val());
+        arg.selectedFactor = parseFloat($(`#${row}_${referenceBy}`).val());
         return arg;
     }
 };
